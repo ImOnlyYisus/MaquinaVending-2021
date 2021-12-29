6af7462d94e024cbbc56c15acde4d2ee6ad0d085
yisusturtle <yisusturtle@gmail.com>
2021-12-29 22:32:29 +0100

    He generado los metodos setters y getters de los atributos, y el to string para mostrar la informacion del objeto que creemos

--- Maquina-Jesus-Alberto/src/main/java/maquina/Productos.java
+++ Maquina-Jesus-Alberto/src/main/java/maquina/Productos.java
@@ -6,9 +6,17 @@ import org.apache.commons.lang3.RandomStringUtils;
 public class Productos {
     private String nombreProducto;
     //COD_PRODUCTO --> EJEMPLO 000, 021, 542...
-    private final String COD_PRODUCTO = RandomStringUtils.randomNumeric(3); //Esto podría introducirse al crear el producto
+    private String codProducto = RandomStringUtils.randomNumeric(3); //Esto podría introducirse al crear el producto
     private double precio = 2.5; //PRECIO POR DEFECTO 2,5€
     private int stock;
+
+    //CONSTRUCTOR PARAMETRIZADO CON (nombre producto, cod producto, precio y stock)
+    public Productos(String nombreProducto, String codProducto, double precio, int stock) {
+        this.nombreProducto = nombreProducto;
+        this.codProducto = codProducto;
+        this.precio = precio;
+        this.stock = stock;
+    }
 
     //CONSTRUCTOR PARAMETRIZADO CON (nombre producto, precio y stock)
     public Productos(String nombreProducto, double precio, int stock) {
@@ -21,5 +29,53 @@ public class Productos {
     public Productos(String nombreProducto) {
         this.nombreProducto = nombreProducto;
 
+    }
+
+    //GETTERS AND SETTERS
+    //-- NOMBRE PRODUCTO --
+    public String getNombreProducto() {
+        return nombreProducto;
+    }
+
+    public void setNombreProducto(String nombreProducto) {
+        this.nombreProducto = nombreProducto;
+    }
+
+    //-- COD_PRODUCTO --
+    public String getCodProducto() {
+        return codProducto;
+    }
+
+    public void setCodProducto(String codProducto) {
+        this.codProducto = codProducto;
+    }
+
+    //-- PRECIO --
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    //-- STOCK --
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    //TO STRING
+    @Override
+    public String toString() {
+        return "Productos{" +
+                "nombreProducto='" + nombreProducto + '\'' +
+                ", codProducto='" + codProducto + '\'' +
+                ", precio=" + precio +
+                ", stock=" + stock +
+                '}';
     }
 }
