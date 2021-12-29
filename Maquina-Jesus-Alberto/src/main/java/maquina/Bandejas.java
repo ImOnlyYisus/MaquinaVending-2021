9517ec9182916648b08929b70ffa483bb1a29527
yisusturtle <yisusturtle@gmail.com>
2021-12-29 23:01:37 +0100

    Creo los getters y los setters, y creo un metodo para introducir un producto y cambiarlo segun el indice que le pasemos, controlando el indice que no sea mayor de la longitud del array y que sea 0 o mayor

--- Maquina-Jesus-Alberto/src/main/java/maquina/Bandejas.java
+++ Maquina-Jesus-Alberto/src/main/java/maquina/Bandejas.java
@@ -20,5 +20,34 @@ public class Bandejas {
         }
     }
 
-    
+    //GETTERS
+    //-- COD BANDEJA --
+    public String getCodBandeja() {
+        return codBandeja;
+    }
+
+    //-- ARRAY PRODUCTOS
+    public Productos[] getArrayProductos() {
+        return arrayProductos;
+    }
+
+    public void setArrayProductos(Productos[] arrayProductos) throws Exception {
+        //MAXIMO 6 TIPOS DE PRODUCTOS EN UNA BANDEJA Y MINIMO 1
+        if(arrayProductos.length<=6 && arrayProductos.length>0) {
+            this.arrayProductos = arrayProductos;
+        } else{
+            throw new Exception("Debes de introducir de 1 a 6 tipos de productos"); //EXCEPCION QUE SALTA
+        }
+    }
+
+    //METODOS
+    //METODO EL CUAL PASAS UN PRODUCTO Y LO INCORPORAS EN LA POSICIÓN INTRODUCIDA
+    public void setProducto(Productos producto, int indice) throws Exception {
+        //Tiene que ser el indice mayor o igual a 0, y dentro del rango de la longitud del array
+        if(indice<=this.arrayProductos.length && indice>=0){
+            this.arrayProductos[indice] = producto;
+        } else{
+            throw new Exception("No se encuentra el indice"); //EXCEPCION QUE SALTA
+        }
+    }
 }
