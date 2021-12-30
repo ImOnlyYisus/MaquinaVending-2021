d5a5019ba39fbe1642021705053724341f2898fe
Alberto <albertogr578@gmail.com>
2021-12-30 11:20:59 +0100

    Añade a la clase Monedero posibles excepciones a los métodos añadir y quitar monedas y billetes

--- Maquina-Jesus-Alberto/src/main/java/maquina/Monedero.java
+++ Maquina-Jesus-Alberto/src/main/java/maquina/Monedero.java
@@ -27,19 +27,6 @@ public class Monedero {
     private int contadorBilletesDiezEuros;
     private int contadorBilletesVeinteEuros;
 
-    public Monedero(int contadorMonedasUnEuro, int contadorMonedasDosEuros, int contadorMonedasUnCentimo, int contadorMonedasDosCentimos, int contadorMonedasCincoCentimos, int contadorMonedasDiezCentimos, int contadorMonedasVeinteCentimos, int contadorMonedasCincuentaCentimos, int contadorBilletesCincoEuros, int contadorBilletesDiezEuros, int contadorBilletesVeinteEuros) {
-        this.contadorMonedasUnEuro = contadorMonedasUnEuro;
-        this.contadorMonedasDosEuros = contadorMonedasDosEuros;
-        this.contadorMonedasUnCentimo = contadorMonedasUnCentimo;
-        this.contadorMonedasDosCentimos = contadorMonedasDosCentimos;
-        this.contadorMonedasCincoCentimos = contadorMonedasCincoCentimos;
-        this.contadorMonedasDiezCentimos = contadorMonedasDiezCentimos;
-        this.contadorMonedasVeinteCentimos = contadorMonedasVeinteCentimos;
-        this.contadorMonedasCincuentaCentimos = contadorMonedasCincuentaCentimos;
-        this.contadorBilletesCincoEuros = contadorBilletesCincoEuros;
-        this.contadorBilletesDiezEuros = contadorBilletesDiezEuros;
-        this.contadorBilletesVeinteEuros = contadorBilletesVeinteEuros;
-    }
 
     public Monedero() {
     }
@@ -64,114 +51,308 @@ public class Monedero {
     }
 
     //Métodos para agregar monedas
-    public void addMonedaUnEuro(int monedas) {
+    public void addMonedaUnEuro(int monedas) throws Exception {
 
-        this.contadorMonedasUnEuro = monedas;
-    }
+        if (monedas < 0) {
 
-    public void addMonedaDosEuros(int monedas) {
+            System.out.println("Introduce un numero de monedas positivo");
 
-       
-        this.contadorMonedasDosEuros = monedas;
-    }
+        } else {
 
-    public void addMonedaUnCentimo(int monedas) {
-
-       
-        this.contadorMonedasUnCentimo = monedas;
-    }
-
-    public void addMonedaDosCentimos(int monedas) {
-
-       
-        this.contadorMonedasDosCentimos = monedas;
-    }
-
-    public void addMonedaCincoCentimos(int monedas) {
-
-     
-        this.contadorMonedasCincoCentimos = monedas;
-    }
-
-    public void addMonedaDiezCentimos(int monedas) {
-
-        this.contadorMonedasDiezCentimos = monedas;
-    }
-
-    public void addMonedaVeinteCentimos(int monedas) {
-
-        this.contadorMonedasVeinteCentimos = monedas;
-    }
-
-    public void addBilleteCincoEuros(int billetes) {
-
-        this.contadorBilletesCincoEuros = billetes;
+            this.contadorMonedasUnEuro = monedas;
+        }
 
     }
 
-    public void addBilleteDiezEuros(int billetes) {
+    public void addMonedaDosEuros(int monedas) throws Exception {
 
-        this.contadorBilletesDiezEuros = billetes;
+        if (monedas < 0) {
+
+            System.out.println("Introduce un numero de monedas positivo");
+
+        } else {
+
+            this.contadorMonedasDosEuros = monedas;
+        }
 
     }
 
-    public void addBilleteVeinteEuros(int billetes) {
+    public void addMonedaUnCentimo(int monedas) throws Exception {
 
-        this.contadorBilletesVeinteEuros = billetes;
+        if (monedas < 0) {
+
+            System.out.println("Introduce un numero de monedas positivo");
+
+        } else {
+
+            this.contadorMonedasUnCentimo = monedas;
+        }
+
+    }
+
+    public void addMonedaDosCentimos(int monedas) throws Exception {
+
+        if (monedas < 0) {
+
+            System.out.println("Introduce un numero de monedas positivo");
+
+        } else {
+
+            this.contadorMonedasDosCentimos = monedas;
+        }
+
+    }
+
+    public void addMonedaCincoCentimos(int monedas) throws Exception {
+
+        if (monedas < 0) {
+
+            System.out.println("Introduce un numero de monedas positivo");
+
+        } else {
+
+            this.contadorMonedasCincoCentimos = monedas;
+        }
+
+    }
+
+    public void addMonedaDiezCentimos(int monedas) throws Exception {
+
+        if (monedas < 0) {
+
+            System.out.println("Introduce un numero de monedas positivo");
+
+        } else {
+
+            this.contadorMonedasDiezCentimos = monedas;
+        }
+
+    }
+
+    public void addMonedaVeinteCentimos(int monedas) throws Exception {
+
+        if (monedas < 0) {
+
+            System.out.println("Introduce un numero de monedas positivo");
+
+        } else {
+
+            this.contadorMonedasVeinteCentimos = monedas;
+        }
+
+    }
+
+    public void addBilleteCincoEuros(int billetes) throws Exception {
+
+        if (billetes < 0) {
+
+            System.out.println("Introduce un numero de billetes positivo");
+
+        } else {
+
+            this.contadorBilletesCincoEuros = billetes;
+        }
+
+    }
+
+    public void addBilleteDiezEuros(int billetes) throws Exception {
+
+        if (billetes < 0) {
+
+            System.out.println("Introduce un numero de billetes positivo");
+
+        } else {
+
+            this.contadorBilletesDiezEuros = billetes;
+        }
+
+    }
+
+    public void addBilleteVeinteEuros(int billetes) throws Exception {
+
+        if (billetes < 0) {
+
+            System.out.println("Introduce un numero de billetes positivo");
+
+        } else {
+
+            this.contadorBilletesVeinteEuros = billetes;
+        }
 
     }
 
     //Métodos para quitar monedas
-    public void removeMonedaUnEuro(int monedas) {
+    public void removeMonedaUnEuro(int monedas) throws Exception {
 
-        this.contadorMonedasUnEuro -= monedas;
-    }
+        if (monedas < 0) {
 
-    public void removeMonedaDosEuros(int monedas) {
+            System.out.println("Introduce un numero de monedas positivo");
+        }
 
-        this.contadorMonedasDosEuros -= monedas;
-    }
+        if (monedas > this.contadorMonedasUnEuro) {
 
-    public void removeMonedaUnCentimo(int monedas) {
+            System.out.println("La resta no puede ser negativa");
 
-        this.contadorMonedasUnCentimo -= monedas;
-    }
+        } else {
 
-    public void removeMonedaDosCentimos(int monedas) {
-
-        this.contadorMonedasDosCentimos -= monedas;
-    }
-
-    public void removeMonedaCincoCentimos(int monedas) {
-
-        this.contadorMonedasCincoCentimos -= monedas;
-    }
-
-    public void removeMonedaDiezCentimos(int monedas) {
-
-        this.contadorMonedasDiezCentimos -= monedas;
-    }
-
-    public void removeMonedaVeinteCentimos(int monedas) {
-
-        this.contadorMonedasVeinteCentimos -= monedas;
-    }
-
-    public void removeBilleteCincoEuros(int billetes) {
-
-        this.contadorBilletesCincoEuros -= billetes;
+            this.contadorMonedasUnEuro -= monedas;
+        }
 
     }
 
-    public void removeBilleteDiezEuros(int billetes) {
+    public void removeMonedaDosEuros(int monedas) throws Exception {
 
-        this.contadorBilletesDiezEuros -= billetes;
+        if (monedas < 0) {
+
+            System.out.println("Introduce un numero de monedas positivo");
+        }
+
+        if (monedas > this.contadorMonedasDosEuros) {
+
+            System.out.println("La resta no puede ser negativa");
+
+        } else {
+
+            this.contadorMonedasDosEuros -= monedas;
+        }
 
     }
 
-    public void removeBilleteVeinteEuros(int billetes) {
+    public void removeMonedaUnCentimo(int monedas) throws Exception {
 
-        this.contadorBilletesVeinteEuros -= billetes;
+        if (monedas < 0) {
+
+            System.out.println("Introduce un numero de monedas positivo");
+        }
+
+        if (monedas > this.contadorMonedasUnCentimo) {
+
+            System.out.println("La resta no puede ser negativa");
+
+        } else {
+
+            this.contadorMonedasUnCentimo -= monedas;
+        }
+    }
+
+    public void removeMonedaDosCentimos(int monedas) throws Exception {
+
+        if (monedas < 0) {
+
+            System.out.println("Introduce un numero de monedas positivo");
+        }
+
+        if (monedas > this.contadorMonedasDosCentimos) {
+
+            System.out.println("La resta no puede ser negativa");
+
+        } else {
+
+            this.contadorMonedasDosCentimos -= monedas;
+        }
+    }
+
+    public void removeMonedaCincoCentimos(int monedas) throws Exception {
+
+        if (monedas < 0) {
+
+            System.out.println("Introduce un numero de monedas positivo");
+        }
+
+        if (monedas > this.contadorMonedasCincoCentimos) {
+
+            System.out.println("La resta no puede ser negativa");
+
+        } else {
+
+            this.contadorMonedasCincoCentimos -= monedas;
+        }
+    }
+
+    public void removeMonedaDiezCentimos(int monedas) throws Exception {
+
+        if (monedas < 0) {
+
+            System.out.println("Introduce un numero de monedas positivo");
+        }
+
+        if (monedas > this.contadorMonedasDiezCentimos) {
+
+            System.out.println("La resta no puede ser negativa");
+
+        } else {
+
+            this.contadorMonedasDiezCentimos -= monedas;
+        }
+    }
+
+    public void removeMonedaVeinteCentimos(int monedas) throws Exception {
+
+        if (monedas < 0) {
+
+            System.out.println("Introduce un numero de monedas positivo");
+        }
+
+        if (monedas > this.contadorMonedasVeinteCentimos) {
+
+            System.out.println("La resta no puede ser negativa");
+
+        } else {
+
+            this.contadorMonedasVeinteCentimos -= monedas;
+        }
+    }
+
+    public void removeBilleteCincoEuros(int billetes) throws Exception {
+
+        if (billetes < 0) {
+
+            System.out.println("Introduce un numero de monedas positivo");
+        }
+
+        if (billetes > this.contadorBilletesCincoEuros) {
+
+            System.out.println("La resta no puede ser negativa");
+
+        } else {
+
+            this.contadorBilletesCincoEuros -= billetes;
+        }
+    }
+
+    public void removeBilleteDiezEuros(int billetes) throws Exception {
+
+        if (billetes < 0) {
+
+            System.out.println("Introduce un numero de monedas positivo");
+        }
+
+        if (billetes > this.contadorBilletesDiezEuros) {
+
+            System.out.println("La resta no puede ser negativa");
+
+        } else {
+
+            this.contadorMonedasDiezCentimos -= billetes;
+        }
+
+    }
+
+    public void removeBilleteVeinteEuros(int billetes) throws Exception {
+
+        if (billetes < 0) {
+
+            System.out.println("Introduce un numero de monedas positivo");
+        }
+
+        if (billetes > this.contadorBilletesVeinteEuros) {
+
+            System.out.println("La resta no puede ser negativa");
+
+        } else {
+
+            this.contadorBilletesVeinteEuros -= billetes;
+        }
 
     }
 
@@ -180,94 +361,49 @@ public class Monedero {
         return contadorMonedasUnEuro;
     }
 
-    public void setContadorMonedasUnEuro(int contadorMonedasUnEuro) {
-        this.contadorMonedasUnEuro = contadorMonedasUnEuro;
-    }
-
     public int getContadorMonedasDosEuros() {
         return contadorMonedasDosEuros;
-    }
-
-    public void setContadorMonedasDosEuros(int contadorMonedasDosEuros) {
-        this.contadorMonedasDosEuros = contadorMonedasDosEuros;
     }
 
     public int getContadorMonedasUnCentimo() {
         return contadorMonedasUnCentimo;
     }
 
-    public void setContadorMonedasUnCentimo(int contadorMonedasUnCentimo) {
-        this.contadorMonedasUnCentimo = contadorMonedasUnCentimo;
-    }
-
     public int getContadorMonedasDosCentimos() {
         return contadorMonedasDosCentimos;
-    }
-
-    public void setContadorMonedasDosCentimos(int contadorMonedasDosCentimos) {
-        this.contadorMonedasDosCentimos = contadorMonedasDosCentimos;
     }
 
     public int getContadorMonedasCincoCentimos() {
         return contadorMonedasCincoCentimos;
     }
 
-    public void setContadorMonedasCincoCentimos(int contadorMonedasCincoCentimos) {
-        this.contadorMonedasCincoCentimos = contadorMonedasCincoCentimos;
-    }
-
     public int getContadorMonedasDiezCentimos() {
         return contadorMonedasDiezCentimos;
-    }
-
-    public void setContadorMonedasDiezCentimos(int contadorMonedasDiezCentimos) {
-        this.contadorMonedasDiezCentimos = contadorMonedasDiezCentimos;
     }
 
     public int getContadorMonedasVeinteCentimos() {
         return contadorMonedasVeinteCentimos;
     }
 
-    public void setContadorMonedasVeinteCentimos(int contadorMonedasVeinteCentimos) {
-        this.contadorMonedasVeinteCentimos = contadorMonedasVeinteCentimos;
-    }
-
     public int getContadorMonedasCincuentaCentimos() {
         return contadorMonedasCincuentaCentimos;
-    }
-
-    public void setContadorMonedasCincuentaCentimos(int contadorMonedasCincuentaCentimos) {
-        this.contadorMonedasCincuentaCentimos = contadorMonedasCincuentaCentimos;
     }
 
     public int getContadorBilletesCincoEuros() {
         return contadorBilletesCincoEuros;
     }
 
-    public void setContadorBilletesCincoEuros(int contadorBilletesCincoEuros) {
-        this.contadorBilletesCincoEuros = contadorBilletesCincoEuros;
-    }
-
     public int getContadorBilletesDiezEuros() {
         return contadorBilletesDiezEuros;
-    }
-
-    public void setContadorBilletesDiezEuros(int contadorBilletesDiezEuros) {
-        this.contadorBilletesDiezEuros = contadorBilletesDiezEuros;
     }
 
     public int getContadorBilletesVeinteEuros() {
         return contadorBilletesVeinteEuros;
     }
 
-    public void setContadorBilletesVeinteEuros(int contadorBilletesVeinteEuros) {
-        this.contadorBilletesVeinteEuros = contadorBilletesVeinteEuros;
-    }
-
     @Override
     public String toString() {
         return "Monedero{" + "contadorMonedasUnEuro=" + contadorMonedasUnEuro + ", contadorMonedasDosEuros=" + contadorMonedasDosEuros + ", contadorMonedasUnCentimo=" + contadorMonedasUnCentimo + ", contadorMonedasDosCentimos=" + contadorMonedasDosCentimos + ", contadorMonedasCincoCentimos=" + contadorMonedasCincoCentimos + ", contadorMonedasDiezCentimos=" + contadorMonedasDiezCentimos + ", contadorMonedasVeinteCentimos=" + contadorMonedasVeinteCentimos + ", contadorMonedasCincuentaCentimos=" + contadorMonedasCincuentaCentimos + ", contadorBilletesCincoEuros=" + contadorBilletesCincoEuros + ", contadorBilletesDiezEuros=" + contadorBilletesDiezEuros + ", contadorBilletesVeinteEuros=" + contadorBilletesVeinteEuros + '}';
     }
-   
-    
+
 }
