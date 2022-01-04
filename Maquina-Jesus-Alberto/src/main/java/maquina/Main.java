b4fbbc94b4ef1580cc68722341983989453a974f
yisusturtle <yisusturtle@gmail.com>
2022-01-04 18:51:55 +0100

    Pruebas con metodos de la clase Maquina, muestro las tarjetas y su información y pruebo toString de maquina

--- Maquina-Jesus-Alberto/src/main/java/maquina/Main.java
+++ Maquina-Jesus-Alberto/src/main/java/maquina/Main.java
@@ -27,14 +27,19 @@ public class Main {
 
         Productos primerProducto = new Productos("Manzana", 2.30, 3);
         Productos segundoProducto = new Productos("Naranjas", 2.30, 3);
-        Productos tercerProducto = new Productos("Peras");
-        Productos cuartoProducto = new Productos("Ciruela");
-        Productos quintoProducto = new Productos("Ciruela");
-        Productos sextoProducto = new Productos("Ciruela");
-        Productos octavoProducto = new Productos("Ciruela");
+        Productos tercerProducto = new Productos("Peras", 3.00, 9);
+        Productos cuartoProducto = new Productos("Ciruela", 4.50, 8);
+        Productos quintoProducto = new Productos("Limon", 1.50, 6);
+        Productos sextoProducto = new Productos("Chocolate",2.60, 5);
+        Productos octavoProducto = new Productos("Cereza", 4, 4);
 
         Bandejas primeraBandeja = new Bandejas(new Productos[]{primerProducto, segundoProducto, tercerProducto, cuartoProducto, quintoProducto});
         Bandejas segundaBaneja = new Bandejas(new Productos[]{sextoProducto, octavoProducto});
+        Bandejas terceraBaneja = new Bandejas(new Productos[]{sextoProducto, octavoProducto});
+        Bandejas cuartaBaneja = new Bandejas(new Productos[]{sextoProducto, octavoProducto});
+        Bandejas quintaBaneja = new Bandejas(new Productos[]{sextoProducto, octavoProducto});
+        Bandejas sextaBaneja = new Bandejas(new Productos[]{sextoProducto, octavoProducto});
+
 
         System.out.println(primeraBandeja);
         System.out.println(primeraBandeja.getArrayProductos()[1].toString());
@@ -43,12 +48,20 @@ public class Main {
         LogsDatos.tablaBandejas(new Bandejas[]{primeraBandeja});
         LogsDatos.tablaMonedero(monedero);
 
-        Maquina maquinaEjemplo = new Maquina(new Bandejas[]{primeraBandeja, segundaBaneja});
+        Maquina maquinaEjemplo = new Maquina(new Bandejas[]{primeraBandeja, segundaBaneja, terceraBaneja, cuartaBaneja, quintaBaneja, sextaBaneja},"Calle x", monedero);
 
         //IMPRIMIR TODOS LOS CODIGOS DE LA MAQUINA
         System.out.println(maquinaEjemplo.numeroProductosMaquina());
         for(int i=0; i< maquinaEjemplo.codigoProducto().length ; i++){
             System.out.println(maquinaEjemplo.codigoProducto()[i]);
+        }
+
+        System.out.println(maquinaEjemplo);
+        for(int i=0; i<maquinaEjemplo.getCVVTarjeta().length; i++){
+            System.out.println("Nº TARJETA: "+ maquinaEjemplo.getNumeroTarjeta()[i]+
+                    " CVV: " + maquinaEjemplo.getCVVTarjeta()[i] +
+                    " FECHA VENCIMIENTO: "+ maquinaEjemplo.getFechaVencimientoTarjeta()[i]
+            );
         }
 
     }
