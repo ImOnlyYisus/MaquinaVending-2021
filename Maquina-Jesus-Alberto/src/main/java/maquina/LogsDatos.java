e5ed39d64f97aeede5ce700d16faa3c17bd7cb64
Alberto <albertogr578@gmail.com>
2022-01-23 18:48:53 +0100

    Añade comentarios a la clase LogDatos

--- Maquina-Jesus-Alberto/src/main/java/maquina/LogsDatos.java
+++ Maquina-Jesus-Alberto/src/main/java/maquina/LogsDatos.java
@@ -1,8 +1,9 @@
 package maquina;
 
+//Clase que se ultilizara para poder visualizar los distintos datos de las clases con formato de tablas
 public class LogsDatos {
 
-    //METODO PARA MOSTRAR TODOS LOS DATOS DE LOS PRODUCTOS EN UNA TABLA
+    //Método para mostrar todos los datos de los productos en una tabla
     public static void tablaProductos(Productos[] arrayProductos){
         String formatoTabla = "│ %-15s │ %-12s │ %-6.2f │ %-5d │%n";
 
@@ -12,6 +13,7 @@ public class LogsDatos {
         System.out.format("│ NOMBRE PRODUCTO │ COD PRODUCTO │ PRECIO │ STOCK │%n");
         System.out.format("├─────────────────┼──────────────┼────────┼───────┤%n");
 
+        //Se recorre el array de productos y se usa System.out.format para devolver una cadena formateada utilizando el formato especificado mostrando la tabla y los productos
         for (int i = 0; i < arrayProductos.length; i++) {
             System.out.format(formatoTabla, arrayProductos[i].getNombreProducto(), arrayProductos[i].getCodProducto(), arrayProductos[i].getPrecio(), arrayProductos[i].getStock());
         }
@@ -19,7 +21,7 @@ public class LogsDatos {
         System.out.format("└─────────────────┴──────────────┴────────┴───────┘%n");
     }
 
-    //METODO PARA MOSTRAR TODOS LOS DATOS DE LOS BANDEJAS EN UNA TABLA
+     //Método para mostrar todos los datos de as bandejas en una tabla
     public static void tablaBandejas(Bandejas[] arrayBandejas){
         String formatoTabla = "│ %-11s │ %-12s │ %-22s │%n";
 
@@ -30,6 +32,7 @@ public class LogsDatos {
         System.out.format("├─────────────┼──────────────┼───────────────────────┤%n");
 
 
+        //Se utiliza un for anidado para poder recorrer el array de bandejas y productos
         for (int i = 0; i < arrayBandejas.length; i++) {
             for(int z = 0; z < arrayBandejas[i].getArrayProductos().length; z++) {
                 System.out.format(formatoTabla, arrayBandejas[i].getCodBandeja(), arrayBandejas[i].getArrayProductos()[z].getCodProducto(), arrayBandejas[i].getArrayProductos()[z].getNombreProducto());
@@ -39,7 +42,7 @@ public class LogsDatos {
         System.out.format("└─────────────┴──────────────┴───────────────────────┘%n");
     }
 
-    //METODO QUE MUESTRA TODOS LOS DATOS IMPORTANTES DE EL MONEDERO
+    //Método que muestra todos los datos importantes de el monedero
     public static void tablaMonedero(Monedero monedero){
         String formatoTabla = "│ %-13d │ %-13d │ %-13d │ %-13d │ %-13d │ %-13d │ %-13d │ %-13d │ %-14d │ %-14d │%n";
 
@@ -49,6 +52,7 @@ public class LogsDatos {
         System.out.format("│ MONEDAS 0.01€ │ MONEDAS 0.05€ │ MONEDAS 0.10€ │ MONEDAS 0.20€ │ MONEDAS 0.50€ │ MONEDAS 1.00€ │ MONEDAS 2.00€ │ BILLETE 5.00€ │ BILLETE 10.00€ │ BILLETE 20.00€ │%n");
         System.out.format("├───────────────┼───────────────┼───────────────┼───────────────┼───────────────┼───────────────┼───────────────┼───────────────┼────────────────┼────────────────┤%n");
 
+        //Se muestran todos los contadores de las monedas y billetes
         System.out.format(formatoTabla,
                 monedero.getContadorMonedasUnCentimo(),
                 monedero.getContadorMonedasDosCentimos(),
@@ -68,11 +72,12 @@ public class LogsDatos {
         System.out.format("│ DINERO TOTAL                                                                                                                                                    │%n");
         System.out.format("├─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────┤%n");
 
+        //Se muestra finalmente la tabla y el dinero total del monedero
         System.out.format(formatoTabla, monedero.getDineroTotal());
         System.out.format("└─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────┘%n");
     }
 
-    //METODO QUE MUESTRA TODOS LOS DATOS IMPORTANTES DE LA MAQUINA
+    //Método que muestra todos los datos importantes de la maquina
     public static void tablaMaquina(Maquina maquina){
         String formatoTabla ="│ %-36s │ %-36s │ %-11d │ %-10d │%n";
         System.out.format("┌────────────────────────────────────────────────────────────────────────────────────────────────────────┐%n");
@@ -90,10 +95,12 @@ public class LogsDatos {
 
         formatoTabla ="│ %-16s │ %-3d │ %-18s │%n";
 
+        //Se recorre el array de tipo LocalDate de la fecha de vencimiento para mostrar los datos de las tarjetas creadas
         for(int i=0; i<maquina.getFechaVencimientoTarjeta().length; i++){
             System.out.format(formatoTabla, maquina.getNumeroTarjeta()[i],maquina.getCVVTarjeta()[i],maquina.getFechaVencimientoTarjeta()[i]);
         }
         System.out.format("└──────────────────┴─────┴────────────────────┘%n");
+        //Se muestan las tablas restantes
         tablaMonedero(maquina.getMonedero());
         tablaBandejas(maquina.getArrayBandejas());
 
