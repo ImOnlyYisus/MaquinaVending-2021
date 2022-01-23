d8bc279192fb6861f5fc270c036aed16ee58ace5
Alberto <albertogr578@gmail.com>
2022-01-23 18:08:13 +0100

    Modifico comentarios de la clase Bandejas

--- Maquina-Jesus-Alberto/src/main/java/maquina/Bandejas.java
+++ Maquina-Jesus-Alberto/src/main/java/maquina/Bandejas.java
@@ -1,70 +1,71 @@
 package maquina;
 
-//UTILIDAD PARA GENERAR RANDOMS
 import org.apache.commons.lang3.RandomStringUtils;
 
 import java.util.Arrays;
 
 public class Bandejas {
+
+    //VARIABLES
+    
+    //Array de productos
     private Productos[] arrayProductos;
-    //COD BANDEJA --> EJEMPLO AGF, WQE, AAA...
+    //El codigo de la bandeja que estara formado por tres letras mayusculas usando la clase RandomStringUtils.randomAlphabetic().toUpperCase().
+    //Ej: AGF, WQE, AAA...
     private String codBandeja = RandomStringUtils.randomAlphabetic(3).toUpperCase();
 
-    //CONSTRUCTOR PARAMETRIZADO CON UN ARRAY DE PRODUCTOS
+    //MÉTODOS
+    
+    //Constructor parametrizado se le pasa un array de productos
     public Bandejas(Productos[] arrayProductos) throws Exception {
-        //MAXIMO 6 TIPOS DE PRODUCTOS EN UNA BANDEJA Y MINIMO 1
-        if(arrayProductos.length<=6 && arrayProductos.length>0) {
+        //La bandeja de productos tendra un maximo de 6 y un minimo de un producto
+        if (arrayProductos.length <= 6 && arrayProductos.length > 0) {
             this.arrayProductos = arrayProductos;
-        } else{
-            throw new IllegalArgumentException("Debes de introducir de 1 a 6 tipos de productos"); //EXCEPCION QUE SALTA
+        } else {
+            //Saltaria una excpeción de tipo IllegalArgumentException
+            throw new IllegalArgumentException("Debes de introducir de 1 a 6 tipos de productos");
         }
     }
 
-    //GETTERS
-    //-- COD BANDEJA --
-    public String getCodBandeja() {
-        return codBandeja;
+    //Metodo en el cual pasas un producto y su indice como parametro y lo incorportas a la posición introducida
+    public void setProducto(Productos producto, int indice) throws Exception {
+        //El indice tiene que ser mayor o igual a 0, y dentro del rango de la longitud del array
+        if (indice <= this.arrayProductos.length && indice >= 0) {
+            this.arrayProductos[indice] = producto;
+        } else {
+            //Saltaria la excepción
+            throw new IllegalArgumentException("No se encuentra el indice"); 
+        }
     }
 
-    //-- ARRAY PRODUCTOS
+    //Getters y setters
     public Productos[] getArrayProductos() {
         return arrayProductos;
     }
 
     public void setArrayProductos(Productos[] arrayProductos) throws Exception {
-        //MAXIMO 6 TIPOS DE PRODUCTOS EN UNA BANDEJA Y MINIMO 1
-        if(arrayProductos.length<=6 && arrayProductos.length>0) {
+        //Misma restricción que en el constructor parametrizado
+        if (arrayProductos.length <= 6 && arrayProductos.length > 0) {
             this.arrayProductos = arrayProductos;
-        } else{
-            throw new IllegalArgumentException("Debes de introducir de 1 a 6 tipos de productos"); //EXCEPCION QUE SALTA
+        } else {
+            throw new IllegalArgumentException("Debes de introducir de 1 a 6 tipos de productos"); 
         }
     }
 
-    //METODOS
-    //METODO EL CUAL PASAS UN PRODUCTO Y LO INCORPORAS EN LA POSICIÓN INTRODUCIDA
-    public void setProducto(Productos producto, int indice) throws Exception {
-        //Tiene que ser el indice mayor o igual a 0, y dentro del rango de la longitud del array
-        if(indice<=this.arrayProductos.length && indice>=0){
-            this.arrayProductos[indice] = producto;
-        } else{
-            throw new IllegalArgumentException("No se encuentra el indice"); //EXCEPCION QUE SALTA
-        }
+    public String getCodBandeja() {
+        return codBandeja;
     }
-
-    //SETTERS
 
     public void setCodBandeja(String codBandeja) {
         this.codBandeja = codBandeja;
     }
 
-
-    //TO STRING
-
+    //toString
     @Override
     public String toString() {
-        return "Bandejas{" +
-                "arrayProductos=" + Arrays.toString(arrayProductos) +
-                ", codBandeja='" + codBandeja + '\'' +
-                '}';
+        return "Bandejas{"
+                + "arrayProductos=" + Arrays.toString(arrayProductos)
+                + ", codBandeja='" + codBandeja + '\''
+                + '}';
     }
 }
